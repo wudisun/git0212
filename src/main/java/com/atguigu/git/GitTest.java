faa3d788d6f90e7699076cec66f8b04f30814f4d
wudi <wudi_9310@163.com>
2022-06-14 21:01:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author w
+ * @create 2022-06-14-20:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println ( "hello git" );
+    }
+}
