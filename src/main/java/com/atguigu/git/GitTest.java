deb58b391f40c18d5e9f1104e78f84e87ebd84be
wudi <wudi_9310@163.com>
2022-06-14 21:58:27 +0800

    push test2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println ( "hello git6" );
         System.out.println ( "hello git5" );
         System.out.println ( "push test" );
+        System.out.println ( "push test2" );
         System.out.println ( "pull test" );
     }
 }
